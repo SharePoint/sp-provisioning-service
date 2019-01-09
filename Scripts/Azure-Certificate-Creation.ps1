184b78d0962c594e0c2e61c9b9ea959c8decbef2
Paolo Pialorsi <paolo@pialorsi.com>
2019-01-09 12:25:58 +0100

    Added comment to PowerShell script

--- Scripts/Azure-Certificate-Creation.ps1
+++ Scripts/Azure-Certificate-Creation.ps1
@@ -1,4 +1,4 @@
-﻿$clearPassword = "P@ssw0rd!"
+﻿$clearPassword = "P@ssw0rd!" # This is a fake password just for sample purposes
 $password = $clearPassword | ConvertTo-SecureString -AsPlainText -Force
 
 $cert = New-PnPAzureCertificate -CommonName "StarterKitProvisioning" -ValidYears 5 -Out C:\temp\StarterKitProvisioning.pfx -CertificatePassword $password
